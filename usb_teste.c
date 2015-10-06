3c479629729628f3d38dfe8cceb307fd8e28d5e2
cuki2 <mauricio@krebs.com.br>
2015-10-06 14:42:05 -0300

    24FJ64GB UART2 OK

--- usb_teste.c
+++ usb_teste.c
@@ -12,11 +12,15 @@
 #use delay(clock=15MHz)
 #pin_select U1TX=PIN_B0
 #pin_select U1RX=PIN_B1
-#use rs232(uart1, baud=9600)
+#pin_select U2TX=PIN_B2
+#pin_select U2RX=PIN_B3
+#use rs232(uart1, baud=9600, stream=IO1)
+#use rs232(uart2, baud=9600, stream=IO2)
 
 int main(void) {
 	while (TRUE) {
-		printf("Hello\n\r");
+		fprintf(IO1, "Hello 1\n\r");
+		fprintf(IO2, "Hello 2\n\r");
 		output_toggle(PIN_B2);
 		delay_ms(1000);
 	}
